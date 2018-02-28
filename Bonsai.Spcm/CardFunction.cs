5df5651a778e4bebdeb04753382577a18295fd67
glopes <glopes@localhost>
2018-02-28 15:34:08 +0000

    Updated Device to report card function and available number of channels.

---
+++ Bonsai.Spcm/CardFunction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Spcm
+{
+    public enum CardFunction
+    {
+        AnalogIn,
+        AnalogOut,
+        DigitalOut,
+        DigitalIn,
+        DigitalIO
+    }
+}
